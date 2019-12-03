384660d3e7b83923cc3ad92181067dc2e41f2c26
RachelDietrich <rdietrich522@gmail.com>
2019-12-02 19:35:37 -0500

    Update ArcadeDrive.java
    just trying

--- 2020_SimpleRobot/src/main/java/frc/robot/oi/ArcadeDrive.java
+++ 2020_SimpleRobot/src/main/java/frc/robot/oi/ArcadeDrive.java
@@ -7,6 +7,19 @@ public class ArcadeDrive {
     public MotorSpeeds calcMotorSpeed(double leftYAxis, double rightXAxis) {
 
         MotorSpeeds motorSpeeds = new MotorSpeeds();
+        
+        if (Math.abs(leftYAxis) > 0 && rightXAxis == 0) {
+        	motorSpeeds.left = leftYAxis;
+        	motorSpeeds.right = leftYAxis * -1;
+        }
+        else if(Math.abs(rightXAxis)>0 && leftYAxis == 0) {
+        	motorSpeeds.left = rightXAxis;
+        	motorSpeeds.right = rightXAxis;
+        }
+        else {
+        	motorSpeeds.left = leftYAxis;
+        	motorSpeeds.right = leftYAxis*-1 / rightXAxis;
+        }
 
         // TODO: Calculate the correct motor speeds
 
