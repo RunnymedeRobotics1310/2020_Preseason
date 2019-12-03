af790c75249b409c5d860bb666f70ec1bfc110cf
Trang-TDSB <trang.tran2@student.tdsb.on.ca>
2019-12-02 19:09:15 -0500

    game controller

--- 2020_SimpleRobot/src/main/java/frc/robot/oi/GameController.java
+++ 2020_SimpleRobot/src/main/java/frc/robot/oi/GameController.java
@@ -4,13 +4,24 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class GameController {
 
-    public static final int A_BUTTON   = 1;
-
+    public static final int A_BUTTON      = 1;
+    public static final int B_BUTTON      = 2;
+    public static final int X_BUTTON      = 3;
+    public static final int Y_BUTTON      = 4;
+    public static final int RIGHT_BUMPER  = 5;
+    public static final int LEFT_BUMPER   = 6;
+    public static final int RIGHT_STICKBUTTON = 7;
+    public static final int LEFT_STICKBUTTON  = 8;
+    public static final int RIGHT_TRIGGER     = 9;
+    public static final int LEFT_TRIGGER      = 10;
+    
+    
     public static final int X_AXIS     = 10;
     public static final int Y_AXIS     = 11;
 
     public static final int LEFT_STICK  = 20;
     public static final int RIGHT_STICK = 21;
+    
 
     private Joystick joystick;
 
@@ -27,7 +38,12 @@ public class GameController {
             if (axis == Y_AXIS) {
                 stickValue = joystick.getRawAxis(1);
             }
+            if (axis == X_AXIS)
+            {
+            	stickValue = joystick.getRawAxis(2);
+            }
         }
+        
 
         if (Math.abs(stickValue) < 0.05) {
             return 0;
