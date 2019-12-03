b2fd707543818eae86e8d48c55b6363de75edd75
RubyMiller <ruby.miller@student.tdsb.on.ca>
2019-12-02 19:58:01 -0500

    inversion

--- 2020_SimpleRobot/src/main/java/frc/robot/Robot.java
+++ 2020_SimpleRobot/src/main/java/frc/robot/Robot.java
@@ -25,104 +25,104 @@ import frc.robot.oi.TankDrive;
  */
 public class Robot extends TimedRobot {
 
-    // User Interface
-    private final GameController driverController = new GameController(RobotMap.DRIVER_CONTROLLER);
+	// User Interface
+	private final GameController driverController = new GameController(RobotMap.DRIVER_CONTROLLER);
 
-    private final TalonSRX leftTalon = new TalonSRX(RobotMap.LEFT_DRIVE_CAN_ADDRESS);
-    private final TalonSRX rightTalon = new TalonSRX(13);
+	private final TalonSRX leftTalon = new TalonSRX(RobotMap.LEFT_DRIVE_CAN_ADDRESS);
+	private final TalonSRX rightTalon = new TalonSRX(13);
 
-    private TankDrive tankDrive = new TankDrive();
-    private ArcadeDrive arcadeDrive = new ArcadeDrive();
+	private TankDrive tankDrive = new TankDrive();
+	private ArcadeDrive arcadeDrive = new ArcadeDrive();
 
-    private DriveSelector driveSelector = new DriveSelector();
+	private DriveSelector driveSelector = new DriveSelector();
 
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any initialization code.
-     */
-    @Override
-    public void robotInit() {
-    }
+	/**
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+	}
 
-    /**
-     * This function is run once each time the robot enters autonomous mode.
-     */
-    @Override
-    public void disabledInit() {
-    }
+	/**
+	 * This function is run once each time the robot enters autonomous mode.
+	 */
+	@Override
+	public void disabledInit() {
+	}
 
-    /**
-     * This function is called periodically during disabled mode.
-     */
-    @Override
-    public void disabledPeriodic() {
-        updateSmartDashboard();
-    }
+	/**
+	 * This function is called periodically during disabled mode.
+	 */
+	@Override
+	public void disabledPeriodic() {
+		updateSmartDashboard();
+	}
 
-    /**
-     * This function is run once each time the robot enters autonomous mode.
-     */
-    @Override
-    public void autonomousInit() {
-    }
+	/**
+	 * This function is run once each time the robot enters autonomous mode.
+	 */
+	@Override
+	public void autonomousInit() {
+	}
 
-    /**
-     * This function is called periodically during autonomous.
-     */
-    @Override
-    public void autonomousPeriodic() {
-        updateSmartDashboard();
-    }
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		updateSmartDashboard();
+	}
 
-    /**
-     * This function is called once each time the robot enters teleoperated mode.
-     */
-    @Override
-    public void teleopInit() {
-    }
+	/**
+	 * This function is called once each time the robot enters teleoperated mode.
+	 */
+	@Override
+	public void teleopInit() {
+	}
 
-    /**
-     * This function is called periodically during teleoperated mode.
-     */
-    @Override
-    public void teleopPeriodic() {
+	/**
+	 * This function is called periodically during teleoperated mode.
+	 */
+	@Override
+	public void teleopPeriodic() {
 
-        double leftYAxis  = driverController.getAxis(GameController.LEFT_STICK,  GameController.Y_AXIS);
-        double rightYAxis = driverController.getAxis(GameController.RIGHT_STICK, GameController.Y_AXIS);
-        double rightXAxis = driverController.getAxis(GameController.RIGHT_STICK, GameController.X_AXIS);
+		double leftYAxis  = driverController.getAxis(GameController.LEFT_STICK,  GameController.Y_AXIS);
+		double rightYAxis = driverController.getAxis(GameController.RIGHT_STICK, GameController.Y_AXIS);
+		double rightXAxis = driverController.getAxis(GameController.RIGHT_STICK, GameController.X_AXIS);
 
-        MotorSpeeds motorSpeeds = null;
+		MotorSpeeds motorSpeeds = null;
 
-        if (driveSelector.getDrive().equals(DriveSelector.TANK_DRIVE)) {
-            motorSpeeds = tankDrive.calcMotorSpeed(leftYAxis, rightYAxis);
-        }
-        else {
-            motorSpeeds = arcadeDrive.calcMotorSpeed(leftYAxis, rightXAxis);
-        }
+		if (driveSelector.getDrive().equals(DriveSelector.TANK_DRIVE)) {
+			motorSpeeds = tankDrive.calcMotorSpeed(leftYAxis, rightYAxis);
+		}
+		else {
+			motorSpeeds = arcadeDrive.calcMotorSpeed(leftYAxis, rightXAxis);
+		}
 
-        leftTalon .set(ControlMode.PercentOutput, motorSpeeds.left);
-        rightTalon.set(ControlMode.PercentOutput, -motorSpeeds.right);
+		leftTalon .set(ControlMode.PercentOutput, -motorSpeeds.left);
+		rightTalon.set(ControlMode.PercentOutput, motorSpeeds.right);
 
-        updateSmartDashboard();
-    }
+		updateSmartDashboard();
+	}
 
-    /**
-     * This function is called once each time the robot enters test mode.
-     */
-    @Override
-    public void testInit() {
-    }
+	/**
+	 * This function is called once each time the robot enters test mode.
+	 */
+	@Override
+	public void testInit() {
+	}
 
-    /**
-     * This function is called periodically during test mode.
-     */
-    @Override
-    public void testPeriodic() {
-        updateSmartDashboard();
-    }
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+		updateSmartDashboard();
+	}
 
-    private void updateSmartDashboard() {
-        SmartDashboard.putNumber("leftDrive",Math.round(driverController.getAxis(GameController.LEFT_STICK, GameController.Y_AXIS)*100)/100.0);
-        SmartDashboard.putNumber("rightDrive",Math.round(driverController.getAxis(GameController.RIGHT_STICK, GameController.Y_AXIS)*100)/100.0);
-    }
+	private void updateSmartDashboard() {
+		SmartDashboard.putNumber("leftDrive",Math.round(driverController.getAxis(GameController.LEFT_STICK, GameController.Y_AXIS)*100)/100.0);
+		SmartDashboard.putNumber("rightDrive",Math.round(driverController.getAxis(GameController.RIGHT_STICK, GameController.Y_AXIS)*100)/100.0);
+	}
 }
