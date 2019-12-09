18110ec1f9717df55cb6bb1b7a022cceb337f7b9
RachelDietrich <rdietrich522@gmail.com>
2019-12-09 17:14:41 -0500

    Update ArcadeDrive.java
    trying it?

--- 2020_SimpleRobot/src/main/java/frc/robot/oi/ArcadeDrive.java
+++ 2020_SimpleRobot/src/main/java/frc/robot/oi/ArcadeDrive.java
@@ -8,24 +8,49 @@ public class ArcadeDrive {
 
         MotorSpeeds motorSpeeds = new MotorSpeeds();
         
-        if (Math.abs(leftYAxis) > 0 && rightXAxis == 0) {
-        	motorSpeeds.left = leftYAxis;
-        	motorSpeeds.right = leftYAxis;
-        }
-        else if(Math.abs(rightXAxis)>0 && leftYAxis == 0) {
-        	motorSpeeds.left = rightXAxis;
-        	motorSpeeds.right = rightXAxis *-1;
-        }
-        
-        else if (Math.abs(rightXAxis)>0 && Math.abs(leftYAxis)>0){
-        	motorSpeeds.left = leftYAxis;
-        	motorSpeeds.right = leftYAxis - rightXAxis;
-        }
-        else {
+        // stop moving
+        if (leftYAxis == 0 && rightXAxis == 0) { 
         	motorSpeeds.left = 0;
         	motorSpeeds.right = 0;
-        	 
+        	return motorSpeeds;
         }
+        // move forward or back
+        if (leftYAxis != 0 && rightXAxis == 0) {
+        	motorSpeeds.left = leftYAxis;
+        	motorSpeeds.right = leftYAxis;
+        	return motorSpeeds;
+        }
+        // spin on spot
+        if(rightXAxis !=0 && leftYAxis == 0) {
+        	motorSpeeds.left = rightXAxis;
+        	motorSpeeds.right = rightXAxis *-1;
+        	return motorSpeeds;
+        }
+        // move forward and turn to the right
+        if (rightXAxis > 0 && leftYAxis >0){
+        	motorSpeeds.left = leftYAxis;
+        	motorSpeeds.right = leftYAxis - rightXAxis;
+        	return motorSpeeds;
+        }
+        // move backward and to the left
+        if (rightXAxis >0 && leftYAxis < 0) {
+        	motorSpeeds.left = leftYAxis * -1;
+        	motorSpeeds.right = leftYAxis * -1 - rightXAxis;
+        	return motorSpeeds;
+        }
+        // move forward and to the left
+        if (rightXAxis <0 && leftYAxis > 0) {
+        	motorSpeeds.left = leftYAxis;
+        	motorSpeeds.right = leftYAxis + rightXAxis;
+        	return motorSpeeds;
+        }
+        //move backward and to the right
+        if (rightXAxis <0 && leftYAxis <0) {
+        	motorSpeeds.left = leftYAxis *-1;
+        	motorSpeeds.right = leftYAxis * -1 + rightXAxis;
+        	return motorSpeeds;
+        }
+        
 
         // TODO: Calculate the correct motor speeds
 
