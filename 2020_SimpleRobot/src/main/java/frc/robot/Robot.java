72e3e26d29ef22eec9f553d141c18007a14be24c
Harkirat Khurana <10066192+hkurana@users.noreply.github.com>
2019-12-09 17:22:02 -0500

    Sensor Update

--- 2020_SimpleRobot/src/main/java/frc/robot/Robot.java
+++ 2020_SimpleRobot/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.oi.ArcadeDrive;
@@ -25,106 +26,112 @@ import frc.robot.oi.TankDrive;
  */
 public class Robot extends TimedRobot {
 
-    // User Interface
-    private final GameController driverController = new GameController(RobotMap.DRIVER_CONTROLLER);
-
-    private final TalonSRX leftTalon = new TalonSRX(RobotMap.LEFT_DRIVE_CAN_ADDRESS);
-    private final TalonSRX rightTalon = new TalonSRX(13);
-
-    private TankDrive tankDrive = new TankDrive();
-    private ArcadeDrive arcadeDrive = new ArcadeDrive();
-
-    private DriveSelector driveSelector = new DriveSelector();
-
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any initialization code.
-     */
-    @Override
-    public void robotInit() {
-    }
-
-    /**
-     * This function is run once each time the robot enters autonomous mode.
-     */
-    @Override
-    public void disabledInit() {
-    }
-
-    /**
-     * This function is called periodically during disabled mode.
-     */
-    @Override
-    public void disabledPeriodic() {
-        updateSmartDashboard();
-    }
-
-    /**
-     * This function is run once each time the robot enters autonomous mode.
-     */
-    @Override
-    public void autonomousInit() {
-    }
-
-    /**
-     * This function is called periodically during autonomous.
-     */
-    @Override
-    public void autonomousPeriodic() {
-        updateSmartDashboard();
-    }
-
-    /**
-     * This function is called once each time the robot enters teleoperated mode.
-     */
-    @Override
-    public void teleopInit() {
-    }
-
-    /**
-     * This function is called periodically during teleoperated mode.
-     */
-    @Override
-    public void teleopPeriodic() {
-
-        double leftYAxis  = driverController.getAxis(GameController.LEFT_STICK,  GameController.Y_AXIS);
-        double rightYAxis = driverController.getAxis(GameController.RIGHT_STICK, GameController.Y_AXIS);
-        double rightXAxis = driverController.getAxis(GameController.RIGHT_STICK, GameController.X_AXIS);
-
-        MotorSpeeds motorSpeeds = null;
-
-        if (driveSelector.getDrive().equals(DriveSelector.TANK_DRIVE)) {
-            motorSpeeds = tankDrive.calcMotorSpeed(leftYAxis, rightYAxis);
         }
-        else {
-            motorSpeeds = arcadeDrive.calcMotorSpeed(leftYAxis, rightXAxis);
-        }
+	// User Interface
+	private final GameController driverController = new GameController(RobotMap.DRIVER_CONTROLLER);
 
-        leftTalon .set(ControlMode.PercentOutput, -motorSpeeds.left);
-        rightTalon.set(ControlMode.PercentOutput, motorSpeeds.right);
+	private final TalonSRX leftTalon = new TalonSRX(RobotMap.LEFT_DRIVE_CAN_ADDRESS);
+	private final TalonSRX rightTalon = new TalonSRX(13);
 
-        updateSmartDashboard();
-    }
+	private TankDrive tankDrive = new TankDrive();
+	private ArcadeDrive arcadeDrive = new ArcadeDrive();
 
-    /**
-     * This function is called once each time the robot enters test mode.
-     */
-    @Override
-    public void testInit() {
-    }
+	private DriveSelector driveSelector = new DriveSelector();
+	private DigitalInput forwardLimit = new DigitalInput(RobotMap.FORWARD_LIMIT_DIO_PORT);
 
-    /**
-     * This function is called periodically during test mode.
-     */
-    @Override
-    public void testPeriodic() {
-        updateSmartDashboard();
-    }
 
-    private void updateSmartDashboard() {
-        SmartDashboard.putString("Driver Controller", driverController.toString());
-        SmartDashboard.putNumber("Left Y Axis",Math.round(driverController.getAxis(GameController.LEFT_STICK, GameController.Y_AXIS)*100)/100.0);
-        SmartDashboard.putNumber("Right Y Axis",Math.round(driverController.getAxis(GameController.RIGHT_STICK, GameController.Y_AXIS)*100)/100.0);
-        SmartDashboard.putNumber("Right X Axis",Math.round(driverController.getAxis(GameController.RIGHT_STICK, GameController.X_AXIS)*100)/100.0);
-    }
+
+	/**
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+	}
+
+	/**
+	 * This function is run once each time the robot enters autonomous mode.
+	 */
+	@Override
+	public void disabledInit() {
+	}
+
+	/**
+	 * This function is called periodically during disabled mode.
+	 */
+	@Override
+	public void disabledPeriodic() {
+		updateSmartDashboard();
+	}
+
+	/**
+	 * This function is run once each time the robot enters autonomous mode.
+	 */
+	@Override
+	public void autonomousInit() {
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		updateSmartDashboard();
+	}
+
+	/**
+	 * This function is called once each time the robot enters teleoperated mode.
+	 */
+	@Override
+	public void teleopInit() {
+	}
+
+	/**
+	 * This function is called periodically during teleoperated mode.
+	 */
+	@Override
+	public void teleopPeriodic() {
+
+		double leftYAxis  = driverController.getAxis(GameController.LEFT_STICK,  GameController.Y_AXIS);
+		double rightYAxis = driverController.getAxis(GameController.RIGHT_STICK, GameController.Y_AXIS);
+		double rightXAxis = driverController.getAxis(GameController.RIGHT_STICK, GameController.X_AXIS);
+
+		MotorSpeeds motorSpeeds = null;
+
+		if (driveSelector.getDrive().equals(DriveSelector.TANK_DRIVE)) {
+			motorSpeeds = tankDrive.calcMotorSpeed(leftYAxis, rightYAxis);
+		}
+		else {
+			motorSpeeds = arcadeDrive.calcMotorSpeed(leftYAxis, rightXAxis);
+		}
+
+		leftTalon .set(ControlMode.PercentOutput, -motorSpeeds.left);
+		rightTalon.set(ControlMode.PercentOutput, motorSpeeds.right);
+
+
+		updateSmartDashboard();
+	}
+
+	/**
+	 * This function is called once each time the robot enters test mode.
+	 */
+	@Override
+	public void testInit() {
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+		updateSmartDashboard();
+	}
+
+	private void updateSmartDashboard() {
+		SmartDashboard.putString("Driver Controller", driverController.toString());
+		SmartDashboard.putNumber("Left Y Axis",Math.round(driverController.getAxis(GameController.LEFT_STICK, GameController.Y_AXIS)*100)/100.0);
+		SmartDashboard.putNumber("Right Y Axis",Math.round(driverController.getAxis(GameController.RIGHT_STICK, GameController.Y_AXIS)*100)/100.0);
+		SmartDashboard.putNumber("Right X Axis",Math.round(driverController.getAxis(GameController.RIGHT_STICK, GameController.X_AXIS)*100)/100.0);
+		SmartDashboard.putBoolean("Forward Limit", forwardLimit.get());
+	}
 }
--- 2020_SimpleRobot/src/main/java/frc/robot/RobotMap.java
+++ 2020_SimpleRobot/src/main/java/frc/robot/RobotMap.java
@@ -2,17 +2,20 @@ package frc.robot;
 
 public class RobotMap {
 
-    /*
-     * Define all of the drive motors
-     */
-    public static final int LEFT_DRIVE_CAN_ADDRESS     = 11;
-    public static final int LEFT_FOLLOWER_CAN_ADDRESS  = 12;
-    public static final int RIGHT_DRIVE_CAN_ADDRESS    = 13;
-    public static final int RIGHT_FOLLOWER_CAN_ADDRESS = 14;
+	/*
+	 * Define all of the drive motors
+	 */
+	public static final int LEFT_DRIVE_CAN_ADDRESS     = 11;
+	public static final int LEFT_FOLLOWER_CAN_ADDRESS  = 12;
+	public static final int RIGHT_DRIVE_CAN_ADDRESS    = 13;
+	public static final int RIGHT_FOLLOWER_CAN_ADDRESS = 14;
 
-    /*
-     * Driver and Operator controller locations (default)
-     */
-    public static final int DRIVER_CONTROLLER = 0;
-    public static final int OPERATOR_CONTROLLER = 1;
+	// Define Sensors
+	public static final int FORWARD_LIMIT_DIO_PORT = 1;
+
+	/*
+	 * Driver and Operator controller locations (default)
+	 */
+	public static final int DRIVER_CONTROLLER = 0;
+	public static final int OPERATOR_CONTROLLER = 1;
 }
