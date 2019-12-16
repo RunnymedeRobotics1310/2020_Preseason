64b33af2305347ecbb1f00cca8d7ee50cc883e9d
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-12-16 17:32:35 -0500

    Refactor examples to Drive

--- 2020_CommandRobot/src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ 2020_CommandRobot/src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -13,8 +13,8 @@ import frc.robot.Robot;
 /**
  * An example command.  You can replace me with your own command.
  */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class DefaultDriveCommand extends Command {
+  public DefaultDriveCommand() {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.m_subsystem);
   }
