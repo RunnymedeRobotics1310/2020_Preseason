461d9182c5f992376de8b96a4fbdfce5f8ea3b28
Shuu <45219546+Shizuekana@users.noreply.github.com>
2019-12-16 18:31:49 -0500

    drive subsystem

--- 2020_CommandRobot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ 2020_CommandRobot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -20,28 +20,28 @@ import frc.robot.commands.DefaultDriveCommand;
  */
 public class DriveSubsystem extends Subsystem {
 
-    private final TalonSRX leftTalon = new TalonSRX(RobotMap.LEFT_DRIVE_CAN_ADDRESS);
-    private final TalonSRX rightTalon = new TalonSRX(13);
-    private DigitalInput forwardLimit = new DigitalInput(RobotMap.FORWARD_LIMIT_DIO_PORT);
+	private final TalonSRX leftTalon = new TalonSRX(RobotMap.LEFT_DRIVE_CAN_ADDRESS);
+	private final TalonSRX rightTalon = new TalonSRX(13);
+	private final DigitalInput forwardLimit = new DigitalInput(RobotMap.FORWARD_LIMIT_DIO_PORT);
 
-    @Override
-    public void initDefaultCommand() {
-        setDefaultCommand(new DefaultDriveCommand());
-    }
+	@Override
+	public void initDefaultCommand() {
+		setDefaultCommand(new DefaultDriveCommand());
+	}
 
-    public void setSpeed(double leftSpeed, double rightSpeed) {
+	public void setSpeed(double leftSpeed, double rightSpeed) {
 
-        if (!forwardLimit.get()) {
-            if (motorSpeeds.left >= 0 || motorSpeeds.right >= 0){
-                motorSpeeds.left = 0;
-                motorSpeeds.right = 0;
-            }
-        }
+		if (!forwardLimit.get()) {
+			if (leftSpeed >= 0 || rightSpeed >= 0){
+				leftSpeed = 0;
+				rightSpeed = 0;
+			}
+		}
 
-        leftTalon .set(ControlMode.PercentOutput, -motorSpeeds.left);
-        rightTalon.set(ControlMode.PercentOutput, motorSpeeds.right);
+		leftTalon.set(ControlMode.PercentOutput, -leftSpeed);
+		rightTalon.set(ControlMode.PercentOutput, rightSpeed);
+
+	}
 
 
-
-    }
 }
