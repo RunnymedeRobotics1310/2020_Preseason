3854ef6f173b8717d2b3c3b02999e511b7d1c0d5
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-12-16 18:25:58 -0500

    Add auto selector

---
+++ 2020_CommandRobot/src/main/java/frc/robot/oi/AutoSelector.java
@@ -0,0 +1,34 @@
+package frc.robot.oi;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class AutoSelector {
+
+    public static SendableChooser<String> autoSelector;
+
+    public static final String NONE      = "None";
+    public static final String FORWARD_1 = "Forward 1m";
+    public static final String FORWARD_2 = "Forward 2m";
+
+    static {
+        // Auto Options
+        autoSelector = new SendableChooser<String>();
+        autoSelector.setDefaultOption(NONE, NONE);
+        autoSelector.addOption(FORWARD_1, FORWARD_1);
+        autoSelector.addOption(FORWARD_2, FORWARD_2);
+
+        SmartDashboard.putData("Auto Selector", autoSelector);
+    }
+
+    public String getSelectedAuto() {
+
+        String selectedAuto = autoSelector.getSelected();
+
+        // If there is nothing selected, then assume arcade
+        if (selectedAuto == null) {
+            return NONE;
+        }
+        return selectedAuto;
+    }
+}
