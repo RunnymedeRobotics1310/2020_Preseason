8a8a582843cbe22a138e1572168aa6510530b74c
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-12-16 19:33:34 -0500

    Only output the driver stick

--- 2020_CommandRobot/src/main/java/frc/robot/OI.java
+++ 2020_CommandRobot/src/main/java/frc/robot/OI.java
@@ -41,9 +41,7 @@ public class OI {
     }
 
     public void periodic() {
-    	SmartDashboard.putString("Driver Controller", driverController.toString());
-    	SmartDashboard.putNumber("Left Stick", GameController.LEFT_STICK);
-    	SmartDashboard.putNumber("Right Stick", GameController.RIGHT_STICK);
+        SmartDashboard.putString("Driver Controller", driverController.toString());
     }
 
 
